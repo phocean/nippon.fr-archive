ee98ff8168e61287bd93fbd53eac0748c9934a3b
phocean <jc@phocean.net>
2019-05-26 21:28:57 +0200

    first commit

---
+++ wp-includes/js/codemirror/htmlhint.js
@@ -0,0 +1,8 @@
+/*!
+ * HTMLHint v0.9.14
+ * https://github.com/yaniswang/HTMLHint
+ *
+ * (c) 2014-2017 Yanis Wang <yanis.wang@gmail.com>.
+ * MIT Licensed
+ */
+var HTMLHint=function(e){function t(e,t){return Array(e+1).js",n)}});
